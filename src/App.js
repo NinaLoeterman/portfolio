bca9ad8d6671393d862c9cc1b5a05808e6df3b2e
Nina Loeterman <nina.loeterman@gmail.com>
2021-01-20 21:18:56 +0200

    react router dom added with basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
+import AboutPage from "./components/AboutPage/AboutPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route path="/about" component={AboutPage} />
+          <Route path="/projects"></Route>
+          <Route path="/resume"></Route>
+          <Route path="/contact"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
