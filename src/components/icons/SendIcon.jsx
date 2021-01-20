e8abd39c35571a50dd7b7a5e6a234c99e9f1dbd9
Nina Loeterman <nina.loeterman@gmail.com>
2021-01-20 21:24:26 +0200

    icons added

---
+++ src/components/icons/SendIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SendIcon = () => {
+  return (
+    <svg
+      width="40"
+      height="44"
+      viewBox="0 0 40 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.25 39.875L38.75 22L1.25 4.125V17.875L26.25 22L1.25 26.125V39.875Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default SendIcon;
