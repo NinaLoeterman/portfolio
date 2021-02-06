d1d377d7fd10a56180dbd1b70484e5341203f362
Nina Loeterman <nina.loeterman@gmail.com>
2021-02-06 18:04:27 +0200

    page title component created

---
+++ src/components/UI/PageTitle/PageTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './PageTitle.styles.css';
+
+const PageTitle = ({ title }) => {
+  return <div className='page-title'>{title}</div>;
+};
+
+export default PageTitle;
--- src/components/pages/ContactPage/ContactPage.jsx
+++ src/components/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import ContactForm from "../../misc/ContactForm/ContactForm";
+import PageTitle from "../../UI/PageTitle/PageTitle";
 
 const ContactPage = () => {
-    return ( 
-        <div>contac meeeeee</div>
-     );
-}
- 
+  return (
+    <div>
+      <PageTitle title="be in touch" />
+      <ContactForm />
+    </div>
+  );
+};
+
 export default ContactPage;
