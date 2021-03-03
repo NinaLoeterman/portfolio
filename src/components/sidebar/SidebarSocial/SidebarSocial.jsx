4808729aaf4c63441e16985a7c1ed265fb0555c5
Nina Loeterman <nina.loeterman@gmail.com>
2021-03-03 08:30:40 +0200

    dockerfile and docker compose added to project

--- src/components/sidebar/SidebarSocial/SidebarSocial.jsx
+++ src/components/sidebar/SidebarSocial/SidebarSocial.jsx
@@ -6,10 +6,20 @@ const SidebarSocial = () => {
   return (
     <div className="sidebar-social">
       <div className="sidebar-social--hr"></div>
-      <a className="sidebar-social--link" href="https://www.linkedin.com/in/nina-loeterman/" target="_blank">
+      <a
+        className="sidebar-social--link"
+        href="https://www.linkedin.com/in/nina-loeterman/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <IconWrapper type="linkedin" />
       </a>
-      <a className="sidebar-social--link" href="https://github.com/NinaLoeterman" target="_blank">
+      <a
+        className="sidebar-social--link"
+        href="https://github.com/NinaLoeterman"
+        target="_blank"
+        rel="noreferrer"
+      >
         <IconWrapper type="github" />
       </a>
     </div>
