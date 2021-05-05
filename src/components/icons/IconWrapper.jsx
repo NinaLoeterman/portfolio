0f4aa592108734b4ddb21a65936fd0f6d290c73b
Nina Loeterman <nina.loeterman@gmail.com>
2021-05-05 22:57:07 +0300

    hover color added to theme and used for social buttons

--- src/components/icons/IconWrapper.jsx
+++ src/components/icons/IconWrapper.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import GithubIcon from "./GithubIcon";
 import LinkedInIcon from "./LinkedInIcon";
 import SendIcon from "./SendIcon";
-import './IconWrapper.styles.css';
+import "./IconWrapper.styles.css";
 
 const IconWrapper = (props) => {
+  const [newProps, setNewProps] = useState({ ...props });
+
   const mapTypeToIcon = {
     github: (props) => <GithubIcon {...props} />,
     linkedin: (props) => <LinkedInIcon {...props} />,
     send: (props) => <SendIcon {...props} />,
   };
 
+  const toggleHover = (fill) => {
+    setNewProps({ ...props, fill });
+  };
+
   return mapTypeToIcon[props.type] ? (
-    <div className="icon-wrapper">{mapTypeToIcon[props.type](props)}</div>
+    <div
+      onMouseOver={() => toggleHover("var(--main-hover")}
+      onMouseOut={() => toggleHover("var(--main-accent")}
+      className="icon-wrapper"
+    >
+      {mapTypeToIcon[newProps.type](newProps)}
+    </div>
   ) : (
     <div>non existant icon type</div>
   );
