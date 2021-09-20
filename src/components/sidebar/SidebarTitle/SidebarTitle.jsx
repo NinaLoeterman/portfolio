8eb9efc690edb6947c1d2ade860918d91cced729
Nina Loeterman <nina.loeterman@gmail.com>
2021-09-20 14:09:51 +0300

    husky test

--- src/components/sidebar/SidebarTitle/SidebarTitle.jsx
+++ src/components/sidebar/SidebarTitle/SidebarTitle.jsx
@@ -9,7 +9,7 @@ const SidebarTitle = () => {
   };
 
   return (
-    <div className='sidebar-title'>
+    <div className="sidebar-title">
       <div className="sidebar-title--name">{text.name}</div>
       <div className="sidebar-title--title">{text.title}</div>
       <div className="sidebar-title--description">{text.description}</div>
