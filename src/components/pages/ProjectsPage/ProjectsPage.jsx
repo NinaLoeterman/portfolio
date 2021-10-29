e6e921999fcc99d39d361a9bcf6c117223f82099
Nina Loeterman <nina.loeterman@gmail.com>
2021-10-29 16:38:05 +0300

    add mobile responsivity

--- src/components/pages/ProjectsPage/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ProjectDisplay from "../../misc/ProjectDisplay/ProjectDisplay.jsx";
 import ProjectsList from "../../misc/ProjectsList/ProjectsList.jsx";
-import "./ProjectsPage.styles.css";
-import axios from "axios";
 import LoadingPage from "../LoadingPage/LoadingPage.jsx";
+import "./ProjectsPage.styles.css";
 
 const ProjectsPage = () => {
   const [projects, setProjects] = useState();
