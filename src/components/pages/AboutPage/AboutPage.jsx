dcbda61383e637d4df00e8121d4885e54d19dbe0
Nina Loeterman <nina.loeterman@gmail.com>
2021-11-20 17:10:51 +0200

    mobile styling

--- src/components/pages/AboutPage/AboutPage.jsx
+++ src/components/pages/AboutPage/AboutPage.jsx
@@ -6,10 +6,10 @@ import "./AboutPage.styles.css";
 const AboutPage = () => {
   return (
     <div className="about-page">
-      <div className="about-page-text about-page-intro">hi, im nina 👋🏼</div>
+      <div className="about-page-text about-page-intro">👋🏼 Hi, I'm nina.</div>
       <div className="about-page-text">
-        Im a developer interested in meaningful tech solutions to real problems.
-        Feel free to look around this site, check out my{" "}
+        I'm a developer interested in meaningful tech solutions to real
+        problems. Feel free to look around this site, check out my{" "}
         <Link to="/projects">projects</Link>, and don’t hesitate to be in{" "}
         <Link to="/contact">touch</Link>
         {":)"}
